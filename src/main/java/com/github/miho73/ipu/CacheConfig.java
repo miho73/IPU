b88fbb74585b052e70dfdefe98204c41a7dd4b87
miho73 <hyrexfiels@gmail.com>
2021-12-21 16:09:39 +0900

    Issue 28 and 29

---
+++ src/main/java/com/github/miho73/ipu/CacheConfig.java
@@ -0,0 +1,22 @@
+package com.github.miho73.ipu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        CacheControl cacheControl = CacheControl.maxAge(31536000, TimeUnit.SECONDS)
+                                                .noTransform()
+                                                .mustRevalidate();
+
+        registry.addResourceHandler("**/univ.*", "**/*.ttf", "**/*.webp", "**/fonts/*.css")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(cacheControl);
+    }
+}
