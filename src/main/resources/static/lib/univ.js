4899f6f14c0d9ee31da7f3968eafaec0f79e8ea7
miho73 <hyrexfiels@gmail.com>
2022-01-06 11:01:36 +0900

    Scroll update

--- src/main/resources/static/lib/univ.js
+++ src/main/resources/static/lib/univ.js
@@ -27,33 +27,15 @@ function hamburger() {
         },100);
     }
     isBurgerOpen = !isBurgerOpen;
-    updateFooter()
 }
 
 window.addEventListener('load', ()=>{
     updateFooterOnInit()
-    gei('footer').style.opacity = 1;
-    document.body.addEventListener('resize', ()=>{
-        setTimeout(function() {
-            updateFooter()
-        }, 10);
-    });
-});
-window.addEventListener('resize', ()=>{
-    updateFooter()
 });
 
-function updateFooter() {
-    if ($(document.body).height() < $(window).height()-81.2) {
-        $('#footer').attr('style', 'position: fixed; bottom: 0px;opacity: 1;');
-    }
-    else {
-        $('#footer').attr('style', 'opacity: 1;');
-    }
-}
 function updateFooterOnInit() {
-    if ($(document.body).height() < $(window).height()-81.2) {
-        $('#footer').attr('style', 'position: fixed; bottom: 0px;');
+    if ($(document.body).height() > $(window).height()) {
+        $('#footer').attr('style', 'opacity:1;');
     }
 }
 
